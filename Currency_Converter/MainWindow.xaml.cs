601304a71e4f8f7b904d00bf60ccbf9dd0c1d661
boris_from_south <sashaborisenko@tut.by>
2023-10-05 17:08:46 +0300

    Add handlers abd comments
    Add handlers for buttons and textbox abd comments for UI and handlers

--- Currency_Converter/MainWindow.xaml.cs
+++ Currency_Converter/MainWindow.xaml.cs
@@ -1,28 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Currency_Converter
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+    // Этот класс is partial потому что состоит из 2 частей - основной(окно) и придаточной(.cs файл)
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            lblCurrency.Content = "Hello World";
         }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            lblCurrency.Content = "";
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            lblCurrency.Content = "Button Clicker";
+        }
+
+        // Обычно для валидации используются 2 отдельных обработчика событий
+        // Первый проверяет введенный текст по нажатию клавиши
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (!int.TryParse(e.Text, out val) && e.Text != "-")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+
+        // Второй метод обрабатывет именно нажатие клавиши (В частности нам интересено нажатие Space)
+        //private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.Key == Key.Space)
+        //    {
+        //        e.Handled = true; // если пробел, отклоняем ввод
+        //    }
+        //}
+
+
     }
 }
