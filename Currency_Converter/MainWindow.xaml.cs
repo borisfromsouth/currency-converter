935499b34758f87409a7f7be046925e6328e35e6
boris_from_south <sashaborisenko@tut.by>
2023-10-06 11:08:50 +0300

    feature: connect values to combobx
    Fill currencies comboboxes with values, modify currency text input

--- Currency_Converter/MainWindow.xaml.cs
+++ Currency_Converter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Currency_Converter
@@ -13,6 +14,8 @@ namespace Currency_Converter
         public MainWindow()
         {
             InitializeComponent();
+
+            BindCurrency();
             lblCurrency.Content = "Hello World";
         }
 
@@ -23,7 +26,30 @@ namespace Currency_Converter
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "Button Clicker";
+            //Create a variable as ConvertedValue with double data type to store currency converted value
+            //double ConvertedValue;
+
+            if (/*txtCurrency.Text == null || */txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtCurrency.Focus();  // поставить фокус на поле ввода валюты
+
+                return;
+            }
+            else if (/*cmbFromCurrency.SelectedValue == null || */cmbFromCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurrency.Focus();
+
+                return;
+            }
+            else if (/*cmbToCurrency.SelectedValue == null || */cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbToCurrency.Focus();
+
+                return;
+            }
         }
 
         // Обычно для валидации используются 2 отдельных обработчика событий
@@ -38,14 +64,40 @@ namespace Currency_Converter
         }
 
         // Второй метод обрабатывет именно нажатие клавиши (В частности нам интересено нажатие Space)
-        //private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if (e.Key == Key.Space)
-        //    {
-        //        e.Handled = true; // если пробел, отклоняем ввод
-        //    }
-        //}
+        private void txtCurrency_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true; // если пробел, отклоняем ввод
+            }
+        }
 
+        private void BindCurrency()
+        {
+            DataTable btCurrency = new DataTable();
 
+            btCurrency.Columns.Add("Text");
+            btCurrency.Columns.Add("Value");
+
+            btCurrency.Rows.Add("--Select--", 0);
+            btCurrency.Rows.Add("INR", 1);
+            btCurrency.Rows.Add("USD", 75);
+            btCurrency.Rows.Add("EUR", 85);
+            btCurrency.Rows.Add("SAR", 20);
+            btCurrency.Rows.Add("POUND", 5);
+            btCurrency.Rows.Add("DEM", 43);
+
+            // В источник данных для combobox ставим представление таблицы
+            cmbFromCurrency.ItemsSource = cmbToCurrency.ItemsSource = btCurrency.DefaultView;
+
+            // делаем привязку объекта таблицы к combobox
+            cmbFromCurrency.DisplayMemberPath = "Text"; // визуальное представление объекта
+            cmbFromCurrency.SelectedValuePath = "Value"; // значение объекта
+            cmbFromCurrency.SelectedIndex = 0; // задаем первичное значение combobox
+
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
     }
 }
